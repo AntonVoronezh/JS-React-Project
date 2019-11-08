47904c13842fcc7fc16da376d0fc6add72c7df6d
AntonVoronezh <pstut@mail.ru>
2019-11-08 18:07:07 +0300

    add src/hoc/

---
+++ src/hoc/index.js
@@ -0,0 +1,8 @@
+import { WithTranslation } from './with-translation';
+import { ErrorBoundary } from './error-boundary';
+import { RouteWithSubRoutes } from './route-with-sub-routes';
+import { WithFields } from './with-fields';
+import { WithModels } from './with-models';
+import { WithSync } from './with-sync';
+
+export { WithTranslation, ErrorBoundary, RouteWithSubRoutes, WithFields, WithModels, WithSync };
