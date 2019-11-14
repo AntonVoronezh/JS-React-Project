c580e6fc55077e75caf251de6de07f7827799573
AntonVoronezh <pstut@mail.ru>
2019-11-14 21:20:09 +0300

    add src/store/redux-lib/ForRedux.js

---
+++ src/store/redux-lib/ForRedux.js
@@ -0,0 +1,11 @@
+import { WithRSAA } from './WithRSAA';
+
+export class ForRedux extends WithRSAA {
+	constructor(props) {
+		super({
+			RSAANames: props.RSAANames,
+			functionsNames: [...props.typesNames, ...props.functionsNames],
+			typesNames: [...props.typesNames, ...props.functionsNames],
+		});
+	}
+}
