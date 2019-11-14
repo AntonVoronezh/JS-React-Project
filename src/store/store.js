c418889f51374a0407147db2e2a9a9176e0a42fc
AntonVoronezh <pstut@mail.ru>
2019-11-14 21:16:18 +0300

    add src/store/

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { apiMiddleware } from 'redux-api-middleware';
+
+import rootReducer from './rootReducer';
+
+let middleware;
+
+if (process.env.NODE_ENV === 'development') {
+	middleware = applyMiddleware(apiMiddleware, thunk, logger);
+} else {
+	middleware = applyMiddleware(apiMiddleware, thunk);
+}
+
+export default () => createStore(rootReducer, middleware);
