df3e98717eaed3fa4f3213830453a76564b410ee
AntonVoronezh <pstut@mail.ru>
2019-11-27 20:35:20 +0300

    add src/store/actions/fields.js

---
+++ src/store/actions/fields.js
@@ -0,0 +1,21 @@
+import { dal } from '../../helpers/libs/dal';
+import { mySchema } from '../../helpers/shemas';
+import { ForRedux } from '../redux-lib';
+
+const fieldsCreators = new ForRedux({
+	RSAANames: [{ type: 'fields' }],
+	functionsNames: ['delete field', 'edit field', 'add field'],
+	typesNames: ['set fields from local storage'],
+});
+
+const fetchFields = () =>
+	dal.get({
+		object: fieldsCreators,
+		endpoint: 'fields',
+		namespace: 'fields',
+		part: 'fields',
+		shema: mySchema,
+	});
+
+
+export { fetchFields, fieldsCreators };
