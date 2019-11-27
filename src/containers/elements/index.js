469776910f3f231139522042aa8bed78423146c9
AntonVoronezh <pstut@mail.ru>
2019-11-27 20:36:11 +0300

    add src/containers/

---
+++ src/containers/elements/index.js
@@ -0,0 +1,4 @@
+export { HeaderElement } from './header-element';
+export { FooterElement } from './footer-element';
+export { FieldsSelect } from './fields-select';
+export { TypeSelect } from './type-select';
