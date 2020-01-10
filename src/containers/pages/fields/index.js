031d432f9c803867ea9d01b37397bedf51151f43
AntonVoronezh <pstut@mail.ru>
2020-01-10 11:28:04 +0300

    add src/containers/pages/fields/index.js

---
+++ src/containers/pages/fields/index.js
@@ -0,0 +1,4 @@
+export { FieldCreatePage } from './field-create-page';
+export { FieldEditPage } from './field-edit-page';
+export { FieldsListPage } from './fields-list-page';
+export { FieldsPage } from './fields-page';
