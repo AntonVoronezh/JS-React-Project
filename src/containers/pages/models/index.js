c31d78fdbadfc1fca1823cc26d90202935cdcdf7
AntonVoronezh <pstut@mail.ru>
2020-01-13 10:43:42 +0300

    add src/containers/pages/models/

---
+++ src/containers/pages/models/index.js
@@ -0,0 +1,4 @@
+export { ModelsPage } from './models-page';
+export { ModelCreatePage } from './model-create-page';
+export { ModelEditPage } from './model-edit-page';
+export { ModelsListPage } from './models-list-page';
