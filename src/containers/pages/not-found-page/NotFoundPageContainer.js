f6719c7e7fb658b281f34d8dd2d7b76eec30f1b9
AntonVoronezh <pstut@mail.ru>
2020-01-13 10:43:04 +0300

    add src/containers/pages/not-found-page/NotFoundPageContainer.js

---
+++ src/containers/pages/not-found-page/NotFoundPageContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import { ErrorBoundary } from '../../../hoc';
+import { NotFoundPage } from '../../../components/pages';
+
+class NotFoundPageContainer extends Component {
+	render() {
+		return (
+			<ErrorBoundary>
+				<NotFoundPage />
+			</ErrorBoundary>
+		);
+	}
+}
+
+export { NotFoundPageContainer };
