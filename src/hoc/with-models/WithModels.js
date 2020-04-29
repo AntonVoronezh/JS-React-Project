0c89a3d84e6332fe8fdecd34061598003c5de667
AntonVoronezh <pstut@mail.ru>
2020-04-29 18:48:48 +0300

    7

--- src/hoc/with-models/WithModels.js
+++ src/hoc/with-models/WithModels.js
@@ -1,20 +1,20 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, {Component, Fragment} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
-import { fetchModels, modelsCreators } from '../../store/actions';
-import { Spinner, FetchError, SyncBtn } from '../../components/elements';
-import { getModelsErrorState, getModelsState } from '../../store/selectors';
-import { ErrorBoundary } from '../../hoc';
+import {fetchModels, modelsCreators} from '../../store/actions';
+import {Spinner, FetchError, SyncBtn222} from '../../components/elements';
+import {getModelsErrorState, getModelsState} from '../../store/selectors';
+import {ErrorBoundary} from '../../hoc';
 import ui from '../../helpers/libs/ui';
 import dal from '../../helpers/libs/dal';
 
-const { setModelsFromLocalStorage } = modelsCreators.getFunctions();
+const {setModelsFromLocalStorage} = modelsCreators.getFunctions();
 
 function WithModels(WrappedComponent) {
 	class Wrap extends Component {
 		componentDidMount() {
-			const { modelsList, fetchModels, setModelsFromLocalStorage, errorMsg } = this.props;
+			const {modelsList, fetchModels, setModelsFromLocalStorage, errorMsg} = this.props;
 
 			!modelsList && ui.getData({ item: 'models', setFunc: setModelsFromLocalStorage, fetchFunc: fetchModels });
 
@@ -33,8 +33,8 @@ function WithModels(WrappedComponent) {
 			if (errorMsg) {
 				return (
 					<Fragment>
-						<FetchError err={errorMsg} />
-						<SyncBtn syncNow={fetchModels} />
+						<FetchError err={errorMsg}/>
+						<SyncBtn222 syncNow={fetchModels}/>
 					</Fragment>
 				);
 			}
