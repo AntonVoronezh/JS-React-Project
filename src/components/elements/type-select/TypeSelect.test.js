a2f88aa991e685c8e3e37856b1e692dac510064b
AntonVoronezh <pstut@mail.ru>
2020-05-04 11:06:38 +0300

    7

---
+++ src/components/elements/type-select/TypeSelect.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Select from 'react-select';
+
+import {TypeSelect} from './TypeSelect';
+import {InputFeedback} from '../../../components/elements/formik-elems/formikElems';
+
+describe('<TypeSelect />', () => {
+	let wrapper;
+	let props = {
+		label: '',
+		typesListForSelect: [],
+		handleChange: () => {
+		},
+		defaultValue: [],
+		error: null,
+	};
+
+	beforeEach(() => (wrapper = shallow(<TypeSelect {...props} />)));
+
+	describe('initial rendering', () => {
+		it('should render one label', () => {
+			expect(wrapper.find('label')).toHaveLength(1);
+		});
+
+		it('should render one Select', () => {
+			expect(wrapper.find(Select)).toHaveLength(1);
+		});
+
+		it('should render 0 InputFeedback', () => {
+			expect(wrapper.find(InputFeedback)).toHaveLength(0);
+		});
+
+		it('should render one InputFeedback when error', () => {
+			props.error = 'Error';
+			wrapper = shallow(<TypeSelect {...props} />);
+
+			expect(wrapper.find(InputFeedback)).toHaveLength(1);
+		});
+	});
+});
