467d3245be02c83930c084b122d0ea00f7ec5714
AntonVoronezh <pstut@mail.ru>
2020-05-06 10:19:17 +0300

    3

---
+++ src/components/pages/main-page/MainPage.jsx
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+import {useTranslation} from 'react-i18next';
+
+const MainPage = () => {
+	const {t} = useTranslation();
+
+	return (
+		<Fragment>
+			<h1 className="display-4">{t('mainPage.title')}</h1>
+			<div>{t('mainPage.text')}</div>
+		</Fragment>
+	);
+};
+
+export {MainPage};
