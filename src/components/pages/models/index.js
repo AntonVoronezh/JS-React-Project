3e0740bdffc7a6d6145b9838dca9ea1680d5b884
AntonVoronezh <pstut@mail.ru>
2020-05-08 10:15:10 +0300

    1

---
+++ src/components/pages/models/index.js
@@ -0,0 +1,4 @@
+export {ModelCreatePage} from './model-create-page';
+export {ModelEditPage} from './model-edit-page';
+export {ModelsListPage} from './models-list-page';
+export {ModelsPage} from './models-page';
