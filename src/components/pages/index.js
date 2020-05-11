1c66c2cccac29e013713ffd33899b9d5aecf62aa
AntonVoronezh <pstut@mail.ru>
2020-05-11 10:52:13 +0300

    1

---
+++ src/components/pages/index.js
@@ -0,0 +1,4 @@
+export {MainPage} from './main-page';
+export {NotFoundPage} from './not-found-page';
+export {ModelCreatePage, ModelEditPage, ModelsListPage, ModelsPage} from './models';
+export {FieldCreatePage, FieldEditPage, FieldsListPage, FieldsPage} from './fields';
