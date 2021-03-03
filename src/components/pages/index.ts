eda4eb5846209afbc048284577db17143575f06c
Anton <pstut@mail.ru>
2021-03-03 17:34:04 +0300

    add types

---
+++ src/components/pages/index.ts
@@ -0,0 +1,14 @@
+export { MainPage } from "./main-page";
+export { NotFoundPage } from "./not-found-page";
+export {
+  ModelCreatePage,
+  ModelEditPage,
+  ModelsListPage,
+  ModelsPage,
+} from "./models";
+export {
+  FieldCreatePage,
+  FieldEditPage,
+  FieldsListPage,
+  FieldsPage,
+} from "./fields";
