a34c503f528557ed93044987315c8c8838f8dda9
Anton <pstut@mail.ru>
2021-03-03 17:32:32 +0300

    add types

---
+++ src/components/pages/fields/fields-list-page/FieldsListPage.tsx
@@ -0,0 +1,109 @@
+// @ts-ignore
+import React, { Fragment } from "react";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import "./FieldsListPage.css";
+import {
+  pagesCount,
+  path,
+  widthsForTable,
+} from "../../../../helpers/constants";
+import { ui } from "../../../../helpers/libs/ui";
+import { bl } from "../../../../helpers/libs/bl";
+import { partsOfWords } from "../../../../helpers/constants";
+
+interface IProps {
+  errorMsg: string;
+  fieldsList: any;
+  deleteField: any;
+  t: any;
+}
+
+const FieldsListPage = ({
+  errorMsg,
+  fieldsList,
+  deleteField,
+  t,
+}: IProps): JSX.Element => {
+  const columns = [
+    {
+      Header: t("fields.list.table.num"),
+      accessor: "id",
+      width: widthsForTable.half,
+      className: "number",
+    },
+    {
+      Header: t("fields.list.table.title"),
+      accessor: "title",
+      Cell: (props) => <strong className="number">{props.value}</strong>,
+    },
+    {
+      Header: t("fields.list.table.name"),
+      accessor: "name",
+    },
+    {
+      Header: t("fields.list.table.actions"),
+      accessor: "id",
+      width: widthsForTable.full,
+      className: "number",
+      Cell: (props) => {
+        const id = props.value;
+
+        const clickHandler = () => {
+          deleteField(id);
+
+          bl.addInObjectForSync({
+            action: partsOfWords.DELETE,
+            part: partsOfWords.FIELDS,
+            values: fieldsList[id].get(),
+          });
+
+          toast(
+            t("toast.text", { type: t("toast.field"), action: t("toast.del") })
+          );
+        };
+
+        return (
+          <Fragment>
+            <NavLink to={`/${path.fields}/${path.edit}/${id}`}>
+              <FontAwesomeIcon icon={faSave} className="edit" />
+            </NavLink>
+            <FontAwesomeIcon
+              icon={faTrashAlt}
+              className="del"
+              // @ts-ignore
+              onClick={clickHandler}
+            />
+          </Fragment>
+        );
+      },
+    },
+  ];
+
+  const list = fieldsList && (
+    <ReactTable
+      defaultPageSize={ui.calcDefaultPagesCountForTable({
+        list: fieldsList,
+        number: pagesCount.FIELDS,
+      })}
+      data={ui.getObjectValues({ object: fieldsList })}
+      columns={columns}
+    />
+  );
+
+  const result = errorMsg ? errorMsg : list;
+
+  return (
+    <Fragment>
+      <h1 className="display-4">{t("fields.list.pageTitle")}</h1>
+      <ul>{result}</ul>
+    </Fragment>
+  );
+};
+
+export { FieldsListPage };
